fd8a345484335b7a2078c3317f24af7a310f5407
Naomi Orji <Orjinaomi92@gmail.com>
2025-08-28 00:56:26 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -11,17 +11,20 @@ function App() {
     </div>
   );
 }
+
 import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
       <UserProfile 
-        name="Naomi" 
-        age="23" 
-        bio="Loves reading and to explore" 
-      /> 
+        name="Alice" 
+        age={25} 
+        bio="Loves hiking and photography" 
+      />
     </div>
   );
 }
+
 export default App;
+
