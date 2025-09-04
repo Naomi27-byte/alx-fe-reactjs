1bf588353efa8100b995631337bc64636c90bddf
Orji Naomi <Orjinaomi92@gmail.com>
2025-09-04 20:28:06 +0100

    created a counter application

--- alx-react-app-new/src/components/App.jsx
+++ alx-react-app-new/src/components/App.jsx
@@ -2,6 +2,8 @@ import Header from './components/Header';
 import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Header />
       <UserProfile name="Naomi" age="22" bio="I love exploring art, culture, and technology." />
       <MainContent />
+      <Counter />
       <Footer />
     </div>
   );
